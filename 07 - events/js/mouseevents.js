6c5fef289e39a72a7fd64b2dd42f83e9b9f1fb0b
cleivas <clara.leivas@gmail.com>
2024-11-19 13:50:50 +0100

    added mousemove code

--- 07 - events/js/mouseevents.js
+++ 07 - events/js/mouseevents.js
@@ -1,6 +1,5 @@
-// Hämta the event source
-const btns = document.querySelectorAll('button');
-
+// 
+const btns = document.querySelectorAll('#buttons button');
 
 btns[0].addEventListener('click', response )
 btns[1].addEventListener('click', response )
@@ -10,15 +9,11 @@ function response(event){
     console.log('knappen klickades!');
     console.log(event.target); //Elementet som klickades
 
-    // event.target.style.backgroundColor = 'hotpink';
-
     const h1 = document.querySelector('h1');
     h1.innerText = event.target.innerText;
 }
 
-// ändra bakgrundsfärg vid hover
-
-
+// ändra bakgrundsfärg vid hover med mousteenter och mouseleave
 const h2s = document.querySelectorAll('h2');
 
 for( const h2 of h2s){
@@ -31,4 +26,15 @@ for( const h2 of h2s){
     h2.addEventListener('mouseleave', event => {
         document.body.style.backgroundColor = 'white';
     });
+}
+
+// mousemove
+const mousemoveDiv = document.querySelector('#mousemove');
+mousemoveDiv.addEventListener('mousemove', changeColor);
+
+function changeColor(event){
+    //Använd musens x-position (event.clientX) för att bestämma bakgrundsfärgen
+    const hue = event.clientX * 0.5; 
+    console.log(hue);
+    event.target.style.backgroundColor = `hsl(${hue}, 70%, 80%)`;
 }
